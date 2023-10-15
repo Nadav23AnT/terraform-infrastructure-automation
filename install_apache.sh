9a48981dbc33eda34fbc45c6022748be3a8b233b
Nadav23AnT <nadav2282@gmail.com>
2023-10-15 16:02:54 +0300

    fixed install_apache.sh

--- install_apache.sh
+++ install_apache.sh
@@ -2,4 +2,4 @@
         sudo apt update -y
         sudo apt install apache2 -y
         sudo systemctl start apache2
-        sudo bash -c 'echo "<h1>created by TerraForm</h1><p>using user_data</p>" > /var/www/html/index.html'
+        echo "<h1>TerraForm Project - Deployed</h1><p>using user_data</p>" | sudo tee /var/www//html/index.html
